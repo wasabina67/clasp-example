1462a1e8152e9e8b95dab378d1eecc09c2883933
wasabina67 <wasabina67@gmail.com>
2024-06-25 14:12:22 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 function myFunction() {
   Logger.log('Hello World!');
 }
+
+function sendEmailToSelf() {
+  var recipient = Session.getActiveUser().getEmail();
+  GmailApp.sendEmail(recipient, 'subject', 'body');
+}
